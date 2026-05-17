4ddb67e1cbaf4e6c3d95ef9c26f9da1c602d4923
Attacktive <tribute2pantera@gmail.com>
2026-05-17 16:59:56 +0900

    feat: add Hilt DI modules

---
+++ app/src/main/java/xyz/attacktive/wallhavend/di/NetworkModule.kt
@@ -0,0 +1,44 @@
+package xyz.attacktive.wallhavend.di
+
+import xyz.attacktive.wallhavend.data.api.WallhavenApiService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideJson(): Json = Json { ignoreUnknownKeys = true }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BASIC
+        })
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient, json: Json): Retrofit = Retrofit.Builder()
+        .baseUrl("https://wallhaven.cc/api/v1/")
+        .client(okHttpClient)
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideWallhavenApiService(retrofit: Retrofit): WallhavenApiService =
+        retrofit.create(WallhavenApiService::class.java)
+}
