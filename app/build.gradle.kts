437d2a6aad1d656458a9f57ec2b6a7d6c56f73fc
Attacktive <tribute2pantera@gmail.com>
2026-06-12 14:49:01 +0900

    chore: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
 		applicationId = "xyz.attacktive.wallhavend"
 		minSdk = 26
 		targetSdk = 37
-		versionCode = 28
-		versionName = "1.6.0"
+		versionCode = 29
+		versionName = "1.6.1"
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
 
