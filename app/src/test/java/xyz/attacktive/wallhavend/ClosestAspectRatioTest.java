9573ccb8f4aacfddbb3dc0ede8bcf351d290dc3f
KaustAbhinand <kaustubhabhinan@gmail.com>
2026-06-15 00:47:48 +0530

    Fixed code style issues

--- app/src/test/java/xyz/attacktive/wallhavend/ClosestAspectRatioTest.java
+++ app/src/test/java/xyz/attacktive/wallhavend/ClosestAspectRatioTest.java
@@ -5,27 +5,27 @@ import xyz.attacktive.wallhavend.domain.model.AppSettingsKt;
 
 public class ClosestAspectRatioTest {
 	@Test
-	public void exactMatch_1080x1920_returns9x16() {
+	public void exactMatch1080x1920Returns9x16() {
 		assertEquals("9x16", AppSettingsKt.closestAspectRatio(1080, 1920));
 	}
 
 	@Test
-	public void nonExactMatch_1080x2400_fallsBackTo9x16() {
+	public void nonExactMatch1080x2400FallsBackTo9x16() {
 		assertEquals("9x16", AppSettingsKt.closestAspectRatio(1080, 2400));
 	}
 
 	@Test
-	public void widescreen_2560x1080_returns21x9() {
+	public void wideScreen2560x1080Returns21x9() {
 		assertEquals("21x9", AppSettingsKt.closestAspectRatio(2560, 1080));
 	}
 
 	@Test
-	public void square_1024x1024_returns1x1() {
+	public void square1024x1024Returns1x1() {
 		assertEquals("1x1", AppSettingsKt.closestAspectRatio(1024, 1024));
 	}
 
 	@Test
-	public void tallRatio_1080x2520_fallsBackTo9x16_notSparseRatio() {
+	public void tallRatio1080x2520FallsBackTo9x16NotSparseRatio() {
 		assertEquals("9x16", AppSettingsKt.closestAspectRatio(1080, 2520));
 	}
 
