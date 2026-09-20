044b5b5e2fd890f1abcbf0a7497ad9df88f97d27
Attacktive <tribute2pantera@gmail.com>
2026-09-20 17:16:10 +0900

    chore: bump version to 2.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
 		applicationId = "xyz.attacktive.wallhavend"
 		minSdk = 26
 		targetSdk = 37
-		versionCode = 51
-		versionName = "2.2.1"
+		versionCode = 52
+		versionName = "2.2.2"
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
 
